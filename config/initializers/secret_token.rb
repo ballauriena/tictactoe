d6545d4226a7410bd00e357e3a3bb11ef11f1265
Laura <lleecrowe@gmail.com>
2014-03-10 18:27:15 -0500

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictactoe::Application.config.secret_key_base = 'bc13a03cc591152abb164de50967051eb2f4d8f7331ef5e4bfac4677e30dd0dd04d03499166da8feb1a014a11b5c27d2dafef03382140fb06fef7edd37c7961a'
