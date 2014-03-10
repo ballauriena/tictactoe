1df50e49661b6a8da6e3181f05d15d9684b0ab12
Laura <lleecrowe@gmail.com>
2014-03-10 18:52:10 -0500

    changed user to player

--- db/migrate/20140310234416_moves.rb
+++ db/migrate/20140310234416_moves.rb
@@ -1,14 +1,14 @@
 class Moves < ActiveRecord::Migration
   def change
   	create_table :moves do |t|
-  		t.references :user
+  		t.references :player
   		t.references :game
   		t.integer :location
 
   		t.timestamps
   	end
 
-  	add_index :moves, :user_id
+  	add_index :moves, :player_id
   	add_index :moves, :game_id
   end
 end
