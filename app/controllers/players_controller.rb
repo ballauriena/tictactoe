438ba287eb1242d7e6ff4b40383cbdac98829b36
Laura <lleecrowe@gmail.com>
2014-03-24 22:02:16 -0500

    built out new and create methods for players and created player_params

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,25 @@
 class PlayersController < ApplicationController
+	def new
+		if current_player
+			redirect_to player_path(current_player)
+		end
+		@player = Player.new
+	end
+
+	def create
+		@player = Player.new(player_params)
+		if @player.save
+			session[:current_player_id] = @player.id
+			redirect_to player_path(@player)
+		else 
+			flash.now[:alert] = "Password and password confirmation must be the same."
+			render 'players/new'
+		end
+	end
+
+	private
+
+	def player_params
+		params.require(:player).permit(:username, :email, :password, :password_confirmation)
+	end
 end
