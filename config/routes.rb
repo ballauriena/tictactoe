ca87e25825efa61b3a50a2f5e97cb9e60baf4389
Laura <lleecrowe@gmail.com>
2014-03-24 21:40:23 -0500

    added routes to create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Tictactoe::Application.routes.draw do
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
