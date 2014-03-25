1e305ea6a7f170f63ebc6c71ad7c32387b61a6d4
Laura <lleecrowe@gmail.com>
2014-03-24 21:46:03 -0500

    added current_player method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_player
+
+  private
+
+  def current_player
+  	@_current_player ||= session[:current_player_id] && Player.find(session[:current_player_id])
+  end
 end
