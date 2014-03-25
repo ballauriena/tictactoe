cac050f4f2f7637f14136875da3cf97c40385f22
Laura <lleecrowe@gmail.com>
2014-03-24 21:55:45 -0500

    added destroy sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Tictactoe::Application.routes.draw do
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy", as: "logout"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
