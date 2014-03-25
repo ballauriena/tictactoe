6fb8933f1d046ae76205a85a1a3c6d6e3cbe97f1
Laura <lleecrowe@gmail.com>
2014-03-24 21:54:01 -0500

    filled out sessions controller with new, create, and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+	def new
+		if current_player
+			redirect to player_path(current_player)
+		end
+	end
+
+	def create
+		player = Player.find_by_username(params[:username])
+		if player && player.authenticate(params[:password])
+			session[:current_player_id] = player.id
+			redirect to player_path(player)
+		else
+			flash.now[:alert] = "Invalid email or password."
+			render 'sessions/new'
+		end
+	end
+
+	def destroy
+		reset_session
+		redirect_to root_url
+	end
 end
