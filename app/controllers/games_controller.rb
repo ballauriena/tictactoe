48970d443783a87725067b3d999f4e9df76f5b4b
Ballauriena <lleecrowe@gmail.com>
2014-04-16 15:25:38 -0500

    added create, show, and game_params to the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,25 @@
 class GamesController < ApplicationController
+
+	def create
+		@game  = Game.new(challenger_id: current_player.id)
+		if @game.save
+			redirect_to game_path(@game)
+		else
+			redirect_to root_url
+		end
+	end
+
 	def index
 	end
+
+	def show
+		@game = Game.find(params[:id])
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:challenger_id, :challengee_id, :win_status, :winner)
+	end
+
 end
