7af76681c4f101051941148ceef673571f490ebc
Ballauriena <lleecrowe@gmail.com>
2014-04-16 17:13:42 -0500

    added comment for next step so i don't forget

--- app/assets/javascripts/gameboard.js
+++ app/assets/javascripts/gameboard.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   	var boxId = this.id;
   	var gameUrl = String(window.location.pathname);
   	console.log(gameUrl);
-
+//ajax method should post to moves create to create a new move when you click on the board, then change the color of the box
   	$.ajax({
   		type: "GET",
   		url: gameUrl,
